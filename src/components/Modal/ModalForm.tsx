47e86e436f554865d16d7bdb3486fc4113dbd15b
unknown <namlshe176301@fpt.edu.vn>
2024-12-05 11:52:17 +0700

    fix cancel update modal & center it

--- src/components/Modal/ModalForm.tsx
+++ src/components/Modal/ModalForm.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Modal, Form } from 'antd'
 
-interface ModalFormProps<T> {
+export interface ModalFormProps<T> {
   isVisible: boolean
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
   onSubmit: (values: T) => void
   initialValues: T | null
   fields: {
@@ -11,10 +12,11 @@ interface ModalFormProps<T> {
     component: React.ReactNode
     rules?: any[]
     description?: string
+    valuePropName?: string // Cho phép undefined
   }[]
 }
 
-function ModalForm<T>({ isVisible, onSubmit, initialValues, fields }: ModalFormProps<T>) {
+function ModalForm<T>({ isVisible, onSubmit, initialValues, fields, setIsModalOpen }: ModalFormProps<T>) {
   const [form] = Form.useForm()
 
   const handleOk = () => {
@@ -22,6 +24,7 @@ function ModalForm<T>({ isVisible, onSubmit, initialValues, fields }: ModalFormP
   }
 
   const handleCancel = () => {
+    setIsModalOpen(!isVisible)
     form.resetFields()
   }
 
@@ -31,7 +34,15 @@ function ModalForm<T>({ isVisible, onSubmit, initialValues, fields }: ModalFormP
   }
 
   return (
-    <Modal title='Edit Item' open={isVisible} onOk={handleOk} onCancel={handleCancel}>
+    <Modal
+      cancelButtonProps={{ style: { display: 'none' } }}
+      okText='Update'
+      title='Edit Item'
+      open={isVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      centered
+    >
       <Form form={form} layout='vertical' initialValues={initialValues || {}} onFinish={handleFinish}>
         {fields.map((field) => (
           <Form.Item
