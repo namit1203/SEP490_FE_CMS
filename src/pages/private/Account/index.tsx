7e193f30ea5da7b18f12eff2508aee2ef430e088
unknown <namlshe176301@fpt.edu.vn>
2024-12-06 13:42:16 +0700

    remove cached file

--- src/pages/private/Account/index.tsx
+++ src/pages/private/Account/index.tsx
@@ -1,9 +1,8 @@
-import { SearchOutlined } from '@ant-design/icons'
-import { Avatar, Button, Input, InputRef, Popconfirm, Space, Table, TableColumnType, TableProps } from 'antd'
-import { FilterDropdownProps } from 'antd/es/table/interface'
-import React, { useRef, useState } from 'react'
-import Highlighter from 'react-highlight-words'
-import { useQueryAccount } from '../../../queries/account'
+import useColumnSearch from '@/hooks/useColumnSearch'
+import { useQueryAccount } from '@/queries/account'
+import renderWithLoading from '@/utils/renderWithLoading'
+import { Avatar, Button, Popconfirm, Space, Table, TableProps } from 'antd'
+import React from 'react'
 
 interface DataType {
   key: string
@@ -15,103 +14,14 @@ interface DataType {
   status: boolean
 }
 
-type DataIndex = keyof DataType
-
 const AccountPage: React.FC = () => {
-  const { data } = useQueryAccount()
-  const [searchText, setSearchText] = useState('')
-  const [searchedColumn, setSearchedColumn] = useState('')
-  const searchInput = useRef<InputRef>(null)
-
-  const handleSearch = (selectedKeys: string[], confirm: FilterDropdownProps['confirm'], dataIndex: DataIndex) => {
-    confirm()
-    setSearchText(selectedKeys[0])
-    setSearchedColumn(dataIndex)
-  }
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters()
-    setSearchText('')
-  }
-
-  const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<DataType> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type='primary'
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size='small'
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button onClick={() => clearFilters && handleReset(clearFilters)} size='small' style={{ width: 90 }}>
-            Reset
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              confirm({ closeDropdown: false })
-              setSearchText((selectedKeys as string[])[0])
-              setSearchedColumn(dataIndex)
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              close()
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />,
-    onFilter: (value, record) =>
-      record[dataIndex]
-        ?.toString()
-        ?.toLowerCase()
-        ?.includes((value as string).toLowerCase()),
-    filterDropdownProps: {
-      onOpenChange(open) {
-        if (open) {
-          setTimeout(() => searchInput.current?.select(), 100)
-        }
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      )
-  })
-
+  const { data, isLoading } = useQueryAccount()
   const columns: TableProps<DataType>['columns'] = [
     {
       title: 'Avatar',
       dataIndex: 'avatar',
       key: 'avatar',
+      align: 'center',
       render: () => (
         <Avatar src={'https://statics.oeg.vn/storage/DEFAULT%20AVATAR%20PROFILE/akirov6.webp'} alt='Avatar' />
       ),
@@ -121,6 +31,8 @@ const AccountPage: React.FC = () => {
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
+      align: 'center',
+      ...useColumnSearch().getColumnSearchProps('email'),
       render: (text) => <a>{text}</a>,
       width: '20%'
     },
@@ -128,32 +40,38 @@ const AccountPage: React.FC = () => {
       title: 'Tên',
       dataIndex: 'fullName',
       key: 'fullName',
+      align: 'center',
       width: '20%',
-      ...getColumnSearchProps('fullName')
+      ...useColumnSearch().getColumnSearchProps('fullName')
     },
     {
       title: 'Số điện thoại',
       dataIndex: 'numberPhone',
       key: 'numberPhone',
-      width: '20%'
+      align: 'center',
+      width: '20%',
+      ...useColumnSearch().getColumnSearchProps('numberPhone')
     },
     {
       title: 'Tên đăng nhập',
       dataIndex: 'username',
       key: 'username',
+      align: 'center',
       width: '20%',
-      ...getColumnSearchProps('username')
+      ...useColumnSearch().getColumnSearchProps('username')
     },
     {
       title: 'Trạng thái',
       dataIndex: 'status',
       key: 'status',
+      align: 'center',
       render: (status) => <p>{status === true ? 'Khả dụng' : 'Không khả dụng'}</p>,
       width: '20%'
     },
     {
       title: 'Action',
       key: 'action',
+      align: 'center',
       render: () => (
         <Space size='middle'>
           <Button type='primary'>Edit</Button>
@@ -172,7 +90,18 @@ const AccountPage: React.FC = () => {
     key: item.id || item.someUniqueField
   }))
 
-  return <Table<DataType> columns={columns} dataSource={dataSource} />
+  return (
+    <>
+      {renderWithLoading({
+        isLoading,
+        content: (
+          <>
+            <Table columns={columns} dataSource={dataSource} />
+          </>
+        )
+      })}
+    </>
+  )
 }
 
 export default AccountPage
