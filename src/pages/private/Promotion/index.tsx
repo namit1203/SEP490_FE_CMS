41d35b89891654efcf0e0f9e2c7e911b73c73517
unknown <namlshe176301@fpt.edu.vn>
2024-12-06 13:43:08 +0700

    remove cached file 3

--- src/pages/private/Promotion/index.tsx
+++ src/pages/private/Promotion/index.tsx
@@ -1,6 +1,8 @@
+import useColumnSearch from '@/hooks/useColumnSearch'
+import { useQueryPromotion } from '@/queries/promotions'
+import renderWithLoading from '@/utils/renderWithLoading'
 import { Button, Popconfirm, Space, Table, TableProps } from 'antd'
 import React from 'react'
-import { useQueryPromotion } from '../../../queries/promotions'
 
 interface DataType {
   key: string
@@ -10,60 +12,78 @@ interface DataType {
   exchangePoint: number
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  {
-    title: 'Code',
-    dataIndex: 'codePromotion',
-    key: 'codePromotion',
-    sorter: (a, b) => a.codePromotion.localeCompare(b.codePromotion),
-    width: '25%'
-  },
-  {
-    title: 'Mô tả',
-    dataIndex: 'description',
-    key: 'description',
-    render: (text) => <a>{text}</a>,
-    width: '25%'
-  },
-  {
-    title: 'Giá trị',
-    dataIndex: 'discount',
-    key: 'discount',
-    sorter: (a, b) => a.discount - b.discount,
-    width: '20%'
-  },
-  {
-    title: 'Số điểm đổi',
-    dataIndex: 'exchangePoint',
-    key: 'exchangePoint',
-    sorter: (a, b) => a.exchangePoint - b.exchangePoint,
-    width: '20%'
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <Space size='middle'>
-        <Button type='primary'>Edit</Button>
-        <Popconfirm title='Are you sure to delete this item?' okText='Yes' cancelText='No'>
-          <Button type='primary' danger>
-            Delete
-          </Button>
-        </Popconfirm>
-      </Space>
-    )
-  }
-]
-
 const PromotionPage: React.FC = () => {
-  const { data } = useQueryPromotion()
+  const { data, isLoading } = useQueryPromotion()
+
+  const columns: TableProps<DataType>['columns'] = [
+    {
+      title: 'Code',
+      dataIndex: 'codePromotion',
+      key: 'codePromotion',
+      align: 'center',
+      ...useColumnSearch().getColumnSearchProps('codePromotion'),
+      sorter: (a, b) => a.codePromotion.localeCompare(b.codePromotion),
+      width: '25%'
+    },
+    {
+      title: 'Mô tả',
+      dataIndex: 'description',
+      key: 'description',
+      align: 'center',
+      ...useColumnSearch().getColumnSearchProps('description'),
+      render: (text) => <a>{text}</a>,
+      width: '25%'
+    },
+    {
+      title: 'Giá trị',
+      dataIndex: 'discount',
+      key: 'discount',
+      align: 'center',
+      sorter: (a, b) => a.discount - b.discount,
+      width: '20%'
+    },
+    {
+      title: 'Số điểm đổi',
+      dataIndex: 'exchangePoint',
+      key: 'exchangePoint',
+      align: 'center',
+      sorter: (a, b) => a.exchangePoint - b.exchangePoint,
+      width: '20%'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      align: 'center',
+      render: () => (
+        <Space size='middle'>
+          <Button type='primary'>Edit</Button>
+          <Popconfirm title='Are you sure to delete this item?' okText='Yes' cancelText='No'>
+            <Button type='primary' danger>
+              Delete
+            </Button>
+          </Popconfirm>
+        </Space>
+      )
+    }
+  ]
 
   const dataSource = data?.map((item: any) => ({
     ...item,
     key: item.id || item.someUniqueField
   }))
 
-  return <Table<DataType> columns={columns} dataSource={dataSource} />
+  return (
+    <>
+      {renderWithLoading({
+        isLoading,
+        content: (
+          <>
+            <Table columns={columns} dataSource={dataSource} />
+          </>
+        )
+      })}
+    </>
+  )
 }
 
 export default PromotionPage
--- src/pages/private/Request/index.tsx
+++ src/pages/private/Request/index.tsx
@@ -1,11 +1,10 @@
-import { SearchOutlined } from '@ant-design/icons'
-import type { InputRef, TableColumnType, TableProps } from 'antd'
-import { Button, Input, Popconfirm, Space, Table } from 'antd'
-import { FilterDropdownProps } from 'antd/es/table/interface'
-import React, { useRef, useState } from 'react'
-import Highlighter from 'react-highlight-words'
-import { ActionType, ActionTypeDescriptions } from '../../../enums/enum'
-import { useQueryRequest } from '../../../queries/request'
+import { ActionType, ActionTypeDescriptions } from '@/enums/enum'
+import useColumnSearch from '@/hooks/useColumnSearch'
+import { useQueryRequest } from '@/queries/request'
+import renderWithLoading from '@/utils/renderWithLoading'
+import type { TableProps } from 'antd'
+import { Button, Popconfirm, Space, Table } from 'antd'
+import React from 'react'
 
 interface DataType {
   key: string
@@ -16,118 +15,31 @@ interface DataType {
   typeId: string | number
 }
 
-type DataIndex = keyof DataType
-
 const RequestPage: React.FC = () => {
-  const { data } = useQueryRequest()
-
-  const [searchText, setSearchText] = useState('')
-  const [searchedColumn, setSearchedColumn] = useState('')
-  const searchInput = useRef<InputRef>(null)
-
-  const handleSearch = (selectedKeys: string[], confirm: FilterDropdownProps['confirm'], dataIndex: DataIndex) => {
-    confirm()
-    setSearchText(selectedKeys[0])
-    setSearchedColumn(dataIndex)
-  }
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters()
-    setSearchText('')
-  }
-
-  const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<DataType> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type='primary'
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size='small'
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button onClick={() => clearFilters && handleReset(clearFilters)} size='small' style={{ width: 90 }}>
-            Reset
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              confirm({ closeDropdown: false })
-              setSearchText((selectedKeys as string[])[0])
-              setSearchedColumn(dataIndex)
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              close()
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />,
-    onFilter: (value, record) =>
-      record[dataIndex]
-        ?.toString()
-        ?.toLowerCase()
-        ?.includes((value as string).toLowerCase()),
-    filterDropdownProps: {
-      onOpenChange(open) {
-        if (open) {
-          setTimeout(() => searchInput.current?.select(), 100)
-        }
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      )
-  })
+  const { data, isLoading } = useQueryRequest()
 
   const columns: TableProps<DataType>['columns'] = [
     {
       title: 'ID',
       dataIndex: 'id',
       key: 'id',
+      align: 'center',
       width: '10%'
     },
     {
       title: 'Tên',
       dataIndex: 'userName',
       key: 'userName',
+      align: 'center',
       width: '20%',
-      ...getColumnSearchProps('userName')
+      ...useColumnSearch().getColumnSearchProps('userName')
     },
     {
       title: 'Mô tả',
       dataIndex: 'description',
       key: 'description',
-      ...getColumnSearchProps('description'),
+      align: 'center',
+      ...useColumnSearch().getColumnSearchProps('description'),
       render: (text) => <a>{text}</a>,
       width: '25%'
     },
@@ -135,12 +47,14 @@ const RequestPage: React.FC = () => {
       title: 'Trạng thái',
       dataIndex: 'note',
       key: 'note',
+      align: 'center',
       width: '20%'
     },
     {
       title: 'Type',
       dataIndex: 'typeId',
       key: 'typeId',
+      align: 'center',
       width: '20%',
       render: (type: ActionType) => <span>{ActionTypeDescriptions[type]}</span>,
       filters: Object.entries(ActionTypeDescriptions).map(([key, value]) => ({
@@ -150,7 +64,7 @@ const RequestPage: React.FC = () => {
       onFilter: (value, record) => record.typeId === value
     },
     {
-      title: 'Action',
+      title: <div style={{ textAlign: 'center' }}>Action</div>,
       key: 'action',
       render: (_, record) => (
         <Space size='middle'>
@@ -175,6 +89,17 @@ const RequestPage: React.FC = () => {
     key: item.id || item.someUniqueField
   }))
 
-  return <Table<DataType> columns={columns} dataSource={dataSource} />
+  return (
+    <>
+      {renderWithLoading({
+        isLoading,
+        content: (
+          <>
+            <Table columns={columns} dataSource={dataSource} />
+          </>
+        )
+      })}
+    </>
+  )
 }
 export default RequestPage
