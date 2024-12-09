822d3a9e57ca2a16d2cb86afae6543856ff92889
unknown <namlshe176301@fpt.edu.vn>
2024-12-09 21:21:02 +0700

    update datatype

--- src/types/DataType.ts
+++ src/types/DataType.ts
@@ -10,6 +10,12 @@ export interface DataType {
   pointStart?: string
   pointEnd?: string
   driverId?: number
+  codePromotion?: string
+  discount?: string
+  endDate?: string
+  exchangePoint?: number
+  imagePromotion?: string
+  startDate?: string
 }
 
 export interface DataTypeVehicle {
@@ -37,4 +43,32 @@ export interface DataTypeCost {
   createdAt: number
   updateAt: number
   status: boolean
+}
+export interface DataTypeFixedCost {
+  id: number | any
+  dateIncurred: string // ISO Date string
+  description: string
+  licensePlate: string
+  lossCostType: string | null // Nullable field
+  price: number
+  vehicleId: number
+  lossCostTypeId?: number
+  vehicleOwner: string | null // Nullable field
+  vehicleOwnerId: number
+}
+
+export interface DataTypeRequest {
+  requestId: number
+  ticketId: number | null // Nullable field
+  vehicleId: number | any
+  driverId: number
+  note: string | null // Nullable field
+  typeRequestId: number
+  typeName: string
+  startLocation: string
+  endLocation: string
+  startTime: string // ISO Date string
+  endTime: string // ISO Date string
+  seats: number | null // Nullable field
+  price: number
 }
