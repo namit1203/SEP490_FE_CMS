1add66b02f7c94a2bf6571bd74d73d81bda5234a
unknown <namlshe176301@fpt.edu.vn>
2024-12-10 17:50:59 +0700

    Update logic Type staff request

--- src/pages/private/Request/AddVehicleForm/index.tsx
+++ src/pages/private/Request/AddVehicleForm/index.tsx
@@ -1,11 +1,26 @@
+import { HttpStatusCode } from '@/constants/httpStatusCode.enum'
 import { ActionType } from '@/enums/enum'
+import { useAddVehicleByStaffMutation, useQueryRequest } from '@/queries/request'
 import { useQueryVehiclesDetails } from '@/queries/vehicle'
 import { DataTypeRequest } from '@/types/DataType'
-import { Button, Col, Form, Row, Table } from 'antd'
-import { useEffect, useMemo } from 'react'
+import { Button, Col, Form, message, Row, Table } from 'antd'
+import { useEffect, useMemo, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddVehicleForm = ({ data }: { data: DataTypeRequest | undefined }) => {
   const [form] = Form.useForm()
+
+  const { data: requestData, refetch } = useQueryRequest()
+
+  const [isCheck, setIsCheck] = useState<boolean>(false)
+
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const [isLoadingDeny, setIsLoadingDeny] = useState<boolean>(false)
+
+  const AddVehicleByStaff = useAddVehicleByStaffMutation()
 
   const { data: vehicleData, refetch: refetchVehicle } = useQueryVehiclesDetails(
     { id: data?.vehicleId },
@@ -17,6 +32,16 @@ const AddVehicleForm = ({ data }: { data: DataTypeRequest | undefined }) => {
   useEffect(() => {
     if (data?.vehicleId) refetchVehicle()
   }, [data?.vehicleId, refetchVehicle])
+
+  const filtered = useMemo(() => {
+    return requestData?.find((item: DataTypeRequest) => item.id === data?.requestId)
+  }, [data?.requestId, requestData])
+
+  useEffect(() => {
+    if (filtered && filtered.status !== isCheck) {
+      setIsCheck(filtered.status)
+    }
+  }, [filtered, isCheck])
 
   const tableData = useMemo(
     () => [
@@ -47,25 +72,63 @@ const AddVehicleForm = ({ data }: { data: DataTypeRequest | undefined }) => {
     []
   )
 
-  const handleFormSubmit = () => {
-    console.log('Form Submitted')
+  const handleFormAction = async (isApprove: boolean, successMessage: string, errorMessage: string) => {
+    if (isLoading || isLoadingDeny) return
+
+    if (isApprove) {
+      setIsLoading(true)
+    } else {
+      setIsLoadingDeny(true)
+    }
+
+    try {
+      const response = await AddVehicleByStaff.mutateAsync({
+        id: data?.requestId ?? null,
+        isApprove
+      })
+
+      if (response.status === HttpStatusCode.Ok) {
+        refetch()
+        message.success(successMessage)
+        navigate('/request')
+      } else {
+        message.error(errorMessage)
+      }
+    } catch (error) {
+      console.error('Error values:', error)
+      message.error(errorMessage)
+    } finally {
+      if (isApprove) {
+        setIsLoading(false)
+      } else {
+        setIsLoadingDeny(false)
+      }
+    }
   }
 
   return (
-    <Form form={form} layout='vertical' onFinish={handleFormSubmit}>
+    <Form form={form} layout='vertical' onFinish={() => handleFormAction(true, 'Accept successfully', 'Accept failed')}>
       <Table columns={columns} dataSource={tableData} pagination={false} bordered />
-      <Row justify='start' gutter={16} style={{ marginTop: '16px' }}>
-        <Col>
-          <Button type='primary' htmlType='submit' style={{ marginRight: '10px' }}>
-            Accept
-          </Button>
-        </Col>
-        <Col>
-          <Button type='primary' htmlType='button' danger>
-            Deny
-          </Button>
-        </Col>
-      </Row>
+      {!isCheck && (
+        <Row justify='start' gutter={16} style={{ marginTop: '16px' }}>
+          <Col>
+            <Button type='primary' htmlType='submit' style={{ marginRight: '10px' }} loading={isLoading}>
+              Accept
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              type='primary'
+              htmlType='button'
+              danger
+              onClick={() => handleFormAction(false, 'Deny successfully', 'Deny failed')}
+              loading={isLoadingDeny}
+            >
+              Deny
+            </Button>
+          </Col>
+        </Row>
+      )}
     </Form>
   )
 }
